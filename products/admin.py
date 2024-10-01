6fdcfcc2eb9fc20d8faa3832255e011699444855
Inbal <doar001@gmail.com>
2024-10-01 11:40:33 +0300

    modify adnin to show categories

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from .models import Product, Category
 
-from products.models import Category, Product
+# Register Category model
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'popularity')  # Display category fields in admin
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+# Register Product model with Category many-to-many field
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'get_categories')  # Display product fields and categories
+    filter_horizontal = ('categories',)  # Adds a multi-select widget for categories
+
+    # Define a custom method to show categories in the product list view
+    def get_categories(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+    
+    get_categories.short_description = 'Categories'
+
+
